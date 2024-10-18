5fa98f681146522049d7894c793463cdbc7ace07
Alex Yang <himself65@outlook.com>
2024-10-18 14:03:25 -0700

    fix: build error

--- packages/www/src/ai/provider.tsx
+++ packages/www/src/ai/provider.tsx
@@ -1,43 +1,12 @@
 "use server";
 import { createAI } from "ai/rsc";
-import OpenAI from "openai";
-import type { ReactNode } from "react";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || "",
-});
+const initialAIState = {}
 
-export async function confirmPurchase(
-  symbol: string,
-  price: number,
-  amount: number,
-) {
-  "use server";
-  // todo
-}
-
-export async function submitUserMessage(content: string) {
-  "use server";
-  // todo
-}
-
-const initialAIState: {
-  role: "user" | "assistant" | "system" | "function";
-  content: string;
-  id?: string;
-  name?: string;
-}[] = [];
-
-const initialUIState: {
-  id: number;
-  display: ReactNode;
-}[] = [];
+const initialUIState = {}
 
 export const AI = createAI({
-  actions: {
-    submitUserMessage,
-    confirmPurchase,
-  },
+  actions: {},
   initialUIState,
   initialAIState,
 });
