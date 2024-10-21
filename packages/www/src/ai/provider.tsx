d781c9356faf8d4477dcd8064da0ead56e41efb7
Alex Yang <himself65@outlook.com>
2024-10-21 14:11:14 -0700

    fix: use separate pnpm

--- packages/www/src/ai/provider.tsx
+++ packages/www/src/ai/provider.tsx
@@ -4,16 +4,16 @@ import {
   SimpleChatEngine,
   ChatMemoryBuffer,
   ChatMessage,
+  ContextChatEngine,
 } from "llamaindex";
+import { FilterOperator } from "llamaindex/vector-store/types";
 import { createAI, createStreamableUI, getMutableAIState } from "ai/rsc";
 import { vectorStore } from "../lib/sql";
 import { ReactNode } from "react";
 import { runAsyncFnWithoutBlocking } from "../lib/utils";
 import { Skeleton } from "../components/ui/skeleton";
 import { BotMessage } from "../components/message";
-import { FilterOperator } from "llamaindex/vector-store/types";
 
-const index = await VectorStoreIndex.fromVectorStore(vectorStore);
 const initialAIState = {
   messages: [],
 } as {
@@ -47,7 +47,7 @@ export const AI = createAI({
         ],
       });
 
-      const simpleChatEngine = new SimpleChatEngine({
+      const chatEngine = new SimpleChatEngine({
         memory: new ChatMemoryBuffer({
           chatHistory: [],
         }),
@@ -62,7 +62,7 @@ export const AI = createAI({
 
       runAsyncFnWithoutBlocking(async () => {
         ui.update("");
-        const response = await simpleChatEngine.chat({
+        const response = await chatEngine.chat({
           message,
           stream: true,
         });
@@ -95,19 +95,20 @@ export const AI = createAI({
     submitIndex: async (message: string, noteId: string) => {
       "use server";
       const aiState = getMutableAIState<typeof AI>();
+      const prevMessages = aiState.get().messages;
       aiState.update({
         ...aiState.get(),
         messages: [
-          ...aiState.get().messages,
+          ...prevMessages,
           {
             role: "user",
             content: message,
           },
         ],
       });
-
-      const queryEngine = index.asQueryEngine({
-        preFilters: {
+      const index = await VectorStoreIndex.fromVectorStore(vectorStore);
+      const retriever = index.asRetriever({
+        filters: {
           filters: [
             {
               key: "metadata",
@@ -117,12 +118,49 @@ export const AI = createAI({
           ],
         },
       });
-      const response = await queryEngine.query({
-        query,
-        stream: true,
+      const chatEngine = new ContextChatEngine({
+        retriever,
+        chatHistory: [...prevMessages],
       });
 
-      // todo
+      const ui = createStreamableUI(
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-full" />
+        </div>,
+      );
+
+      runAsyncFnWithoutBlocking(async () => {
+        ui.update("");
+        const response = await chatEngine.chat({
+          message,
+          stream: true,
+        });
+        let content = "";
+
+        for await (const { delta } of response) {
+          content += delta;
+          ui.append(delta);
+        }
+
+        aiState.done({
+          ...aiState.get(),
+          messages: [
+            ...aiState.get().messages,
+            {
+              role: "assistant",
+              content,
+            },
+          ],
+        });
+
+        ui.done();
+      });
+
+      return {
+        id: Date.now(),
+        display: <BotMessage>{ui.value}</BotMessage>,
+      };
     },
   },
   initialUIState,
